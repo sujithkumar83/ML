50882dfb5068638baf8b2c1f6aca40195d746873
Sujith Kumar <sujith.kumar83@outlook.com>
2019-07-27 09:05:41 +0530

    With Polynomial Regression

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial Regression.py
@@ -0,0 +1,16 @@
+#Polynomial Regression Example
+
+#import libraries
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#import data
+dset=pd.read_csv('Position_Salaries.csv')
+
+print(type(dset))
+#Divide the dataset into Dependent and Indepenedent variables
+
+print("enter")
+
